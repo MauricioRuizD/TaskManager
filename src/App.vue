a10b062b78de2828376837c1df6929ddd06653b3
MauricioRuizD <mauricioruizdiaz@gmail.com>
2023-02-02 08:23:26 -0500

    side panel icono sin borde

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@
               </div>
           </div>
           <main class="col ps-md-2 pt-2">
-            <a href="#" data-bs-target="#sidebar" data-bs-toggle="collapse" class="border rounded-1 p-1 text-decoration-none"><i class="bi bi-list bi-lg py-2 p-1"></i> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <a href="#" data-bs-target="#sidebar" data-bs-toggle="collapse" class=" p-1 text-decoration-none"><i class="bi bi-list bi-lg py-2 p-1"></i> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
 </svg></a>
             <router-view/>
